0404c280fc57c624f97a00fdeda61cf555c50280
Ayo Isaiah <ayo@ayoisaiah.com>
2016-01-28 11:43:42 +0100

    Fixed some bugs

--- js-calculator/script.js
+++ js-calculator/script.js
@@ -11,40 +11,35 @@ $(document).ready(function () {
 	$(".calc-btn").click(function () {
 		operation.push($(this).text());
 		console.log(operation);
-	});
-
-
-
-	//Clear and Equals
-
-	$(".calc-btn").click(function(event) {
-		/* Act on the event */
 		joined = operation.join("");
 		console.log(joined);
 		$(".output").html("<p>" + joined + "</p>");
-	});	
+	});
+
+	//Clear and Equal to
 
 	$("#btn-equals").click(function () {
 
 		var answer;
 
 		try {
-			answer = eval(joined);
+			answer = eval(joined).toFixed(2);
 			$(".output").empty().html("<p>" + answer + "</p>");
 			$(".history").append("<p>" + joined  + "<span>" + " = "  + answer + "</span>" + "</p>");
 			console.log(answer);
 			operation = [answer];
 		}
 
-		catch (e) {
-			answer = "Error";
+		catch (e) { //Catching Errors
+			answer = "Syntax Error";
 			$(".output").html(answer);
 			operation = [];
 		}
 
 	});
 
-	$("#btn-clear").click(function () {
+	$("#btn-clear").click(
+		function () {
 		$(".output").empty();
 		operation = [];
 	});
