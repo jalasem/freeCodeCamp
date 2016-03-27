2f60b102fe08f6eb9edfd862327ffc9fd6e2a1d7
Ayo Isaiah <ayo@ayoisaiah.com>
2016-03-27 01:28:26 +0100

    [Weather Project]: User can search for weather conditionsof different locations

--- weather/js/script.js
+++ weather/js/script.js
@@ -1,4 +1,84 @@
-var longitude, latitude;
+//Skycons
+
+var skycons = new Skycons({"color": "black"});
+
+skycons.add("animated-icon", Skycons.CLEAR_DAY);
+
+skycons.play();
+
+//Some Global variables
+
+var longitude, latitude, timeHour, timeFull;
+
+//Function to update weather information
+
+function updateWeather (json) {
+
+	longitude = json.coord.lon;
+	latitude = json.coord.lat;
+
+	//AJAX request
+
+	$.getJSON('http://api.geonames.org/timezoneJSON?lat=' + latitude + '&lng=' + longitude + '&username=ayoisaiah', function(timezone) {
+			var rawTimeZone = JSON.stringify(timezone);
+			var parsedTimeZone = JSON.parse(rawTimeZone);
+			var dateTime = parsedTimeZone.time;
+			timeFull = dateTime.substr(11);
+			$(".local-time").html(timeFull); //Update local time
+			timeHour = dateTime.substr(-5, 2);
+	});
+
+	//Update Weather parameters and location
+
+	$(".weather-condition").html(json.weather[0].description);
+	var temp = [(json.main.temp - 273.15).toFixed(0) + "°C", (1.8 * (json.main.temp - 273.15) + 32).toFixed(0) + "F"];
+	$(".temperature").html((json.main.temp - 273.15).toFixed(0) + "°C");
+	// setInterval(function () {
+	// 	var i = 0;
+	// 	$(".temperature").html(temp[1]);
+	// }, 2000);
+	$(".location").html(json.name);
+
+	//Update Weather animation based on the returned weather description
+
+	var weather = json.weather[0].description;
+	
+	if(weather.indexOf("rain") >= 0) {
+		skycons.set("animated-icon", Skycons.RAIN);
+	}
+
+	else if (weather.indexOf("sunny") >= 0) {
+		skycons.set("animated-icon", Skycons.CLEAR_DAY);
+	}
+
+	else if (weather.indexOf("clear") >= 0) {
+		if (timeHour >= 7 && timeHour < 20) {
+			skycons.set("animated-icon", Skycons.CLEAR_DAY);
+		}
+
+		else {
+			skycons.set("animated-icon", Skycons.CLEAR_NIGHT);
+		}		
+	}
+
+	else if (weather.indexOf("cloud") >= 0) {
+		if (timeHour >= 7 && timeHour < 20) {
+			skycons.set("animated-icon", Skycons.PARTLY_CLOUDY_DAY);
+		}
+
+		else {
+			skycons.set("animated-icon", Skycons.PARTLY_CLOUDY_NIGHT);
+		}	
+	}
+
+	else if (weather.indexOf("thunderstorm") >= 0) {
+		skycons.set("animated-icon", Skycons.SLEET);
+	}
+
+	else if (weather.indexOf("snow") >= 0) {
+		skycons.set("animated-icon", Skycons.SNOW);
+	}
+}
 
 //Check for Geoloaction support 
 
@@ -10,24 +90,37 @@ if (navigator.geolocation) {
 	var currentPosition;
 	function getCurrentLocation (position) {
 		currentPosition = position;
-		document.getElementById("longitude").innerHTML = currentPosition.coords.longitude;
-		document.getElementById("latitude").innerHTML = currentPosition.coords.latitude;
 		latitude = currentPosition.coords.latitude;
 		longitude = currentPosition.coords.longitude;
 
 		//AJAX request
 
 		$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&APPID=188b68e6b443a5380ce7ee0f0bb49cfc", function (data) {
-			$("#message").html(JSON.stringify(data));
-			var parsed = $.parseJSON(data);
-			console.log(parsed[0]);
+			var rawJson = JSON.stringify(data);
+			var json = JSON.parse(rawJson);
+			updateWeather(json); //Update Weather parameters
 		});
 	}
 
 	navigator.geolocation.getCurrentPosition(getCurrentLocation);
 	
-
 	};
+
+	//Find a Forcast
+	
+	$("form").on("submit", function(event) {
+		event.preventDefault();
+		var city = $(".find-forcast").val(); //Get value from form input
+		document.getElementById("my-form").reset();
+		
+		//AJAX Request
+
+		$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=188b68e6b443a5380ce7ee0f0bb49cfc", function (data) {
+			var rawJson = JSON.stringify(data);
+			var json = JSON.parse(rawJson);
+			updateWeather(json); //Update Weather parameters
+		});
+	});
 }
 
 //If Geolocation is not supported by the browser, alert the user
