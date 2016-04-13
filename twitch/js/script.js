640bb8816e891feabb564aa7f0f496b3aeb8fa1b
Ayo Isaiah <ayo@ayoisaiah.com>
2016-04-13 13:27:47 +0100

    [Twitch]: Basic design and functions completed

--- twitch/js/script.js
+++ twitch/js/script.js
@@ -1,40 +1,68 @@
 window.onload = function () {
-
-	var twitchStreamers = ["freecodecamp", "faceittv", "comster404", "brunofin", "castro_1021", "lpmassive", "imaqtpie", "bluestacksinc", "savjz", "jacksofamerica", "morganfreemin804" ];
+	var username, url, picture;
+	var twitchStreamers = ["dreamhackcs", "skyzhar", "freecodecamp", "faceittv", "comster404", "brunofin", "terakilobyte", "robotcaleb", "sheevergaming", "esl_sc2", "ogamingsc2", "jacksofamerica"];
 
 	for (var i = 0; i < twitchStreamers.length; i++) {
 		ajax();
 	}
 
-		function ajax () {
-			$.ajax({
-				url: "https://api.twitch.tv/kraken/streams/" + twitchStreamers[i] + "?callback=?",
-				dataType: "jsonp",
-				data: {
-					format: "json"
-				},
+	function appendDiv (section) {
+		$(section).append('<div class="twitch"><div class="row"><div class="one-third column"><div class="image-holder"></div></div><div class="two-thirds column"><span>' + username + '</span></div></div></div>');
+	}
 
-				success: function (data) {
+	function ajax () {
+		$.ajax({
+			url: "https://api.twitch.tv/kraken/streams/" + twitchStreamers[i] + "?callback=?",
+			dataType: "jsonp",
+			data: {
+				format: "json"
+			},
 
-					if (data.stream === null) {
-						console.log(data);
-						console.log(twitchStreamers[i] + " is currently offline");
-					}
+			success: function (data) {
 
-					else if (data.status == 422) {
-						console.log(data);
-						console.log(data.message);
-					}
-
-					else {
-						console.log(data.stream.channel.display_name + " is currently streaming " + data.stream.game);
-					}
-				},
-
-				error: function () {
-					console.log("unable to access json");
+				if (data.stream === null) {
+					url = data._links.channel.substr(38);
+					console.log(data);
+					username = "Channel " + "'<a href='https://twitchtv.com/" + url + "'>" + url+ "</a>'" + " is currently offline"; //https://api.twitch.tv/kraken/channels/
+					appendDiv(".offline");
 				}
-			});
-		}
+
+				else if (data.status == 422) {
+					username = data.message;
+					console.log(data);
+					console.log(data.message);
+					appendDiv(".unavailable");
+				}
+
+				else {
+					console.log(data);
+					picture = 'url(' + data.stream.channel.logo + ')';
+					$(".online .image-holder").css('background', picture);
+					url = data._links.channel.substr(38);
+					username = "<a href='https://twitchtv.com/" + url + "'>" + data.stream.channel.display_name + "</a>" + " is currently streaming " + data.stream.game;
+					console.log(username);
+					appendDiv(".online");
+				}
+			},
+
+			error: function () {
+				console.log("unable to access json");
+			}
+		});
+	}
+
+	$(".online-users").click(function () {
+		$(".online").removeClass('hidden');
+		$(".offline, .unavailable").addClass('hidden');
+	});
+
+	$(".offline-users").click(function () {
+		$(".offline, .unavailable").removeClass('hidden');
+		$(".online").addClass('hidden');
+	});
+
+	$(".all-users").click(function () {
+		$(".online, .offline, .unavailable").removeClass('hidden');	
+	});
 
 };
