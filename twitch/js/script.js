c742d29c0ef593f2650401cff4bf38ceeed678a9
Ayo Isaiah <ayo@ayoisaiah.com>
2016-04-14 11:57:29 +0100

    [Twitch]: Show images for online users

--- twitch/js/script.js
+++ twitch/js/script.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-	var username, url, picture;
+	var username, url, picture, x = 0;
 	var twitchStreamers = ["dreamhackcs", "skyzhar", "freecodecamp", "faceittv", "comster404", "brunofin", "terakilobyte", "robotcaleb", "sheevergaming", "esl_sc2", "ogamingsc2", "jacksofamerica"];
 
 	for (var i = 0; i < twitchStreamers.length; i++) {
@@ -7,7 +7,14 @@ window.onload = function () {
 	}
 
 	function appendDiv (section) {
-		$(section).append('<div class="twitch"><div class="row"><div class="one-third column"><div class="image-holder"></div></div><div class="two-thirds column"><span>' + username + '</span></div></div></div>');
+		$(section).append('<div class="twitch"><div class="row"><div class="one-third column"><div class="image-holder" id="user-image-' + x + '"></div></div><div class="two-thirds column"><span>' + username + '</span></div></div></div>');		
+		if (section == ".online") { //If users are online, load profile images
+			$("#user-image-" + x).css({
+				background: picture,
+				'background-size': '55px'
+			});
+		}
+		x++;
 	}
 
 	function ajax () {
@@ -23,7 +30,7 @@ window.onload = function () {
 				if (data.stream === null) {
 					url = data._links.channel.substr(38);
 					console.log(data);
-					username = "Channel " + "'<a href='https://twitchtv.com/" + url + "'>" + url+ "</a>'" + " is currently offline"; //https://api.twitch.tv/kraken/channels/
+					username = "Channel " + "'<a href='https://twitchtv.com/" + url + "' target='_blank'" + "'>" + url+ "</a>'" + " is currently offline"; //https://api.twitch.tv/kraken/channels/
 					appendDiv(".offline");
 				}
 
@@ -36,10 +43,10 @@ window.onload = function () {
 
 				else {
 					console.log(data);
-					picture = 'url(' + data.stream.channel.logo + ')';
-					$(".online .image-holder").css('background', picture);
+					picture = 'url("' + data.stream.channel.logo + '")';
+					console.log(picture);
 					url = data._links.channel.substr(38);
-					username = "<a href='https://twitchtv.com/" + url + "'>" + data.stream.channel.display_name + "</a>" + " is currently streaming " + data.stream.game;
+					username = "<a href='https://twitchtv.com/" + url + "' target='_blank'" + "'>" + data.stream.channel.display_name +  "</a>" + " is currently streaming " + data.stream.game;
 					console.log(username);
 					appendDiv(".online");
 				}
@@ -51,17 +58,25 @@ window.onload = function () {
 		});
 	}
 
+	//Buttons
+
 	$(".online-users").click(function () {
+		$(".offline-users, .all-users").removeClass('focus');
+		$(".online-users").addClass('focus');
 		$(".online").removeClass('hidden');
 		$(".offline, .unavailable").addClass('hidden');
 	});
 
 	$(".offline-users").click(function () {
+		$(".online-users, .all-users").removeClass('focus');
+		$(".offline-users").addClass('focus');
 		$(".offline, .unavailable").removeClass('hidden');
 		$(".online").addClass('hidden');
 	});
 
 	$(".all-users").click(function () {
+		$(".offline-users, .online-users").removeClass('focus');
+		$(".all-users").addClass('focus');
 		$(".online, .offline, .unavailable").removeClass('hidden');	
 	});
 
